3df0dcce635b055807d15b2897a673e16da4212e
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-01-04 06:04:34 -0600

    .h->.hpp

---
+++ LeetCode/src/unsolved/823/BinaryTreesWithFactors.hpp
@@ -0,0 +1,24 @@
+//
+// #include <algorithm>
+// #include <cmath>
+// #include <iostream>
+// #include <string>
+// #include <vector>
+// struct BinaryNode {
+//   BinaryNode *leftChild = nullptr;
+//   BinaryNode *rightChild = nullptr;
+//   int data = 0;
+// };
+// class BinaryTree {
+//  public:
+//   BinaryTree();
+//   BinaryTree(BinaryNode *Head);
+//
+//  private:
+//   BinaryNode *Head;
+//   std::size_t numOfTrees;
+// };
+// class Solution {
+//  public:
+//   int numFactoredBinaryTrees(std::vector<int> &arr);
+// };
