63af557bbeeadab11b255939c75dc1e8b4695b9b
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-01-03 22:37:02 -0600

    refactor the stucture

---
+++ LeetCode/include/3/LongestSubstring.hpp
@@ -0,0 +1,10 @@
+#ifndef CPPPROJECTS_LEETCODE_UNSOLVED_3_LONGESTSUBSTRING_HPP_
+#define CPPPROJECTS_LEETCODE_UNSOLVED_3_LONGESTSUBSTRING_HPP_
+
+#include <cstddef>
+#include <cstring>
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+#endif  // CPPPROJECTS_LEETCODE_UNSOLVED_3_LONGESTSUBSTRING_HPP_
